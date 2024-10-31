404a2dcd34c4e7c3c736e4782513f943d65683ab
Fall1ingStars <mete0r_@outlook.com>
2024-10-31 16:37:01 +0800

    This is a test from LJW

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
-    // Git operation 1: You need to fix follow code. Add you name and sutdent id after that.
-    // std::cout << "Hello, World!" << std::endl;
-    // std::cout << "Hello, World!" << std::endl;
-    // std::cout << "Hello, World!" << std::endl;
+    Git operation 1: You need to fix follow code. Add you name and sutdent id after that.
+    std::cout << "this from ljw!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
