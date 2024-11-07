d9bdb2b20ca350b10166863cefa25caee3e41433
chz2012 <53894684+chz2012@users.noreply.github.com>
2024-11-07 19:13:58 +0800

    refactor(main.cpp): add a cz test comment
    First to try cz commit tools. It looks perfect.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
     std::cout << "Hello, World!" << std::endl;
     // Git operation 1: You need to fix follow code. Add you name and sutdent id after that.
     std::cout << "This change frome lql " << std::endl;
+    // I need cz test
     // std::cout << "Hello, World!" << std::endl;
     // std::cout << "Hello, World!" << std::endl;
     return 0;
